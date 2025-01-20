087b35ad0890761d197cd1c7a5c8eec5c9064aa3
Edgar <edgar@mercurr.com>
2025-01-20 10:41:08 +0100

    [docker] Remove amd64

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,4 +4,4 @@ docker buildx create --use
 timestamp=$(date +%Y%m%d.%H%M)
 specificVersionTag=mercurr/interval-server:$timestamp
 # Build the docker image + pushes it to registry
-docker buildx build --output=type=registry --platform=linux/amd64,linux/arm64 -t mercurr/interval-server:latest -t $specificVersionTag .
+docker buildx build --output=type=registry --platform=linux/arm64 -t mercurr/interval-server:latest -t $specificVersionTag .
